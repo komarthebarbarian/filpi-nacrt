73ee5af8282fba0ab89df8f9ce9493702cca3559
komarthebarbarian <ajdukmare@gmail.com>
2023-04-20 23:14:08 +0200

    almost done

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//LANG
+$('[lang="en"]').hide();
+$('[lang="sr"]').show();
+$(".language").click(function () {
+  $("*:lang(sr)").toggle();
+  $("*:lang(en)").toggle();
+});
+
 // NAVIGATION
 $(document).ready(function () {
   $(".sidenav").sidenav();
@@ -5,6 +13,56 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $(".carousel").carousel({
-    indicators: true,
+    indicators: false,
   });
 });
+
+//SLIDER
+$(document).ready(function () {
+  $(".slider").slider({
+    indicators: false,
+    duration: 3000,
+    interval: 2000,
+  });
+});
+
+//MATERIAL BOX
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+});
+
+//GALLERY
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
